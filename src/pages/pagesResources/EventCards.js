17fcc259d4d87c3d3f959a1b22533a70ba086a78
AkshitaAgr <s3933878@student.rmit.edu.au>
2022-07-12 16:34:14 +1000

    edit 15

---
+++ src/pages/pagesResources/EventCards.js
@@ -0,0 +1,127 @@
+/* eslint-disable */
+
+import React, { useState } from 'react';
+import "./EventCards.css";
+
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+
+import { Typography } from '@material-ui/core';
+
+//import ScavengerHuntBanner from './images/EventBanners/ScavengerHunt.png';
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import { faTicketSimple } from "@fortawesome/free-solid-svg-icons";
+
+const EventCards = (props) => {
+
+    const [open, setOpen] = useState(false);
+
+    const openDialog = () => {
+        setOpen(true);
+    };
+
+    const closeDialog = () => {
+        setOpen(false);
+    };
+
+    const useStyles = makeStyles({
+        root: {
+          maxWidth: 300
+        },
+        media: {
+          height: "230px"
+        }
+    });
+
+    const classes = useStyles();
+
+    return (
+        <div class = 'eventsCards'>
+            <Card 
+                class = 'eventCard'
+                className={classes.root} 
+                onClick={openDialog}
+                onBackdropClick={closeDialog}
+                style = {{cursor: "pointer", backgroundColor: "#D9D9D9"}}>
+                        
+                <CardMedia
+                    className={classes.media}
+                    image={props.cardImage}
+                    title={props.cardImageTitle}
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        {props.cardContentTitle}
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        {props.cardContentDesc}
+                        <br />
+                        Click to view more details.
+                    </Typography>
+                </CardContent>
+            </Card>
+                    
+            <Dialog 
+                open={open} 
+                onClose={closeDialog}
+                style={{width: 580, marginLeft: "30%"}} >
+
+                <DialogTitle style={{textAlign: "center", backgroundColor: "#EC6C09", color: "white" }} >{props.DialogTitle}</DialogTitle>
+                <DialogContent>
+                    <img src = {props.DialogImage} alt = "Event Banner" height="230" width="460"    />
+                    <br />
+
+                    <Typography color="textSecondary" className="MuiInputLabel-shrink" style={{transform: "translate(0, 1.5px) scale(0.95)", transformOrigin: "top left"}}>
+                        <br />
+                        Description
+                    </Typography>
+                        {props.DialogDesc}
+
+                    <Typography color="textSecondary" className="MuiInputLabel-shrink" style={{transform: "translate(0, 1.5px) scale(0.95)", transformOrigin: "top left"}}>
+                        <br />
+                        Date
+                    </Typography>
+                        {props.DialogDate}
+
+                    <Typography color="textSecondary" className="MuiInputLabel-shrink" style={{transform: "translate(0, 1.5px) scale(0.95)", transformOrigin: "top left"}}>
+                        <br />
+                        Time
+                    </Typography>
+                        {props.DialogTime}  
+
+                    <Typography color="textSecondary" className="MuiInputLabel-shrink" style={{transform: "translate(0, 1.5px) scale(0.95)", transformOrigin: "top left"}}>
+                        <br />
+                        Venue
+                    </Typography>
+                        <a class='eventLink' href={props.VenueLink} target={"_blank"} rel='noreferrer' >
+                            {props.DialogVenue} &nbsp;&nbsp;
+                            <FontAwesomeIcon icon={faLocationDot} style={{color: "#EC6C09", fontSize: "1.25rem"}}/> 
+                        </a>
+
+                    <Typography color="textSecondary" className="MuiInputLabel-shrink" style={{transform: "translate(0, 1.5px) scale(0.95)", transformOrigin: "top left"}}>
+                        <br />
+                        Tickets
+                    </Typography>
+                        <a class='eventLink' href={props.TicketLink} target={"_blank"} rel='noreferrer'>
+                            Get Your Tickets Here! &nbsp;&nbsp;
+                            <FontAwesomeIcon icon={faTicketSimple} style={{color: "#EC6C09", fontSize: "1.25rem"}}/> 
+                            <br />
+                            <br />
+                        </a>
+                </DialogContent>
+                <Button onClick={closeDialog} style={{backgroundColor: "#EC6C09", color: "white"}}>Close</Button>
+            </Dialog> 
+        </div>
+    )
+}
+
+export default EventCards;
--- src/pages/upcomingevents.js
+++ src/pages/upcomingevents.js
@@ -1,29 +1,17 @@
 /* eslint-disable */ 
 
-import React, { useState } from 'react';
+import React from 'react';
 import { Helmet } from "react-helmet";
 import "./pagesCSS/upcomingEvents.css";
 
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogTitle from '@material-ui/core/DialogTitle';
-
-import { Typography } from '@material-ui/core';
-
 import ScavengerHuntBanner from './pagesResources/images/EventBanners/ScavengerHunt.png';
+import EngineeringBallBanner from './pagesResources/images/EventBanners/EngineeringBall.jpg';
+import EngineeringBallCardBanner from './pagesResources/images/EventBanners/EngineeringBallCard.jpg';
+import NetworkingNightBanner from './pagesResources/images/EventBanners/NetworkingNight.jpg';
+
+import EventCards from './pagesResources/EventCards';
 
 const Events = () => {
-
-    const [open, setOpen] = useState(false);
-
-    const openDialog = () => {
-        setOpen(true);
-    };
-
-    const closeDialog = () => {
-        setOpen(false);
-    };
 
 
     return (
@@ -38,42 +26,67 @@ const Events = () => {
                 <br />
                 <h1>Upcoming Events</h1>
                 <br />
-                <Button onClick={openDialog}><img src = {ScavengerHuntBanner} alt = "Scavenger Hunt Banner" height="230"/><br /></Button>
+                
+                <div class = 'eventCards'>
 
-                <Dialog 
-                    open={open} 
-                    onBackdropClick={closeDialog}
-                    style={{position: "absolute"}} >
+                    <div class = 'eventsRow'>
 
-                    <DialogTitle style={{textAlign: "center"}} >FIRE+ Scavenger Hunt</DialogTitle>
-                    <DialogContent style={{textAlign: "center"}}>
-                        *Adding Description Here*
-                        <Typography color="textSecondary" className="MuiInputLabel-shrink" style={{transform: "translate(0, 1.5px) scale(0.95)", transformOrigin: "top left", marginLeft: "9%"}}>
-                        <br />
-                            Date
-                        </Typography>
-                        4 August 2022
-                        <Typography color="textSecondary" className="MuiInputLabel-shrink" style={{transform: "translate(0, 1.5px) scale(0.95)", transformOrigin: "top left", marginLeft: "9%"}}>
-                        <br />
-                            Time
-                        </Typography>
-                        4:30 pm - 8:00 pm
-                        <Typography color="textSecondary" className="MuiInputLabel-shrink" style={{transform: "translate(0, 1.5px) scale(0.95)", transformOrigin: "top left", marginLeft: "9%"}}>
-                        <br />
-                            Venue
-                        </Typography>
-                        <a href="https://www.google.com/maps/place/Garden+Building+RMIT+University/@-37.8080571,144.9641693,17z/data=!3m1!4b1!4m6!3m5!1s0x6ad6439030e6c711:0x9e8c8a7cc366da0e!8m2!3d-37.8080571!4d144.9641693!16s%2Fg%2F11snq0wn3h?authuser=0&hl=en" target={"_blank"} rel='noreferrer'>Garden Building RMIT</a>
-                        <Typography color="textSecondary" className="MuiInputLabel-shrink" style={{transform: "translate(0, 1.5px) scale(0.95)", transformOrigin: "top left", marginLeft: "9%"}}>
-                        <br />
-                            Tickets
-                        </Typography>
-                        $5 dollar tickets <br />
-                        <a href="https://l.facebook.com/l.php?u=https%3A%2F%2Fevents.humanitix.com%2Fthe-fire-scavenger-hunt%3Ffbclid%3DIwAR3aapRCf-trE-LcY_VV_ZOWd62VeJWWZR9vA5ZgLqPLaNddr8YW8uaovzU&h=AT0kbVU-rK_gzzB_0ELFjnfq4bbRpqTV2HYBZbawRnKBkkK46U41YQ6D7sy2efDcNT-wO6CS0CjbrFfxMNo4gLVVbiRkmGRT7M6t1S-1xAv5nDjdR0R71QVwx4QO_pfizOcMOg" target={"_blank"} rel='noreferrer'>Link to the Tickets</a>
-                    </DialogContent>
-                    <Button onClick={closeDialog}>Close</Button>
-                </Dialog> 
-                <br />
-                <br />
+                        <div class = 'eventsColumn'>
+                            <EventCards 
+                                cardImage = {ScavengerHuntBanner}
+                                cardImageTitle = "Scavenger Hunt"
+                                cardContentTitle = "Scavenger Hunt"
+                                cardContentDesc = "To celebrate a start of a new semester, FIRE+ will be hosting our own Scavenger Hunt!"
+                                DialogTitle = "FIRE+ Scavenger Hunt"
+                                DialogImage = {ScavengerHuntBanner}
+                                DialogDesc = "To celebrate a start of a new semester, FIRE+ will be hosting our own Scavenger Hunt! Starting with a Live Bingo session and concluding with a race for the FIRE+ code-word, we've got all aspects of a fun evening covered. Food and Soft Drinks are provided, and prizes are on offer!"
+                                DialogDate = "4 August 2022"
+                                DialogTime = "4:30 pm - 8:00 pm"
+                                VenueLink = "https://www.google.com/maps/place/Garden+Building+RMIT+University/@-37.8080571,144.9641693,17z/data=!3m1!4b1!4m6!3m5!1s0x6ad6439030e6c711:0x9e8c8a7cc366da0e!8m2!3d-37.8080571!4d144.9641693!16s%2Fg%2F11snq0wn3h?authuser=0&hl=en"
+                                DialogVenue = "RMIT Garden Building"
+                                TicketLink = "https://events.humanitix.com/the-fire-scavenger-hunt?fbclid=IwAR0KiDr77qSNrOFCWp8rk0_HcP2zJjI5231BU6MS0Mh6KBSfJE6f37wWCWM"
+                            />
+                        </div>
+
+                        <div class = 'eventsColumn'>
+                            <EventCards 
+                                cardImage = {EngineeringBallCardBanner}
+                                cardImageTitle = "Engineering Ball Banner"
+                                cardContentTitle = "Engineering Ball"
+                                cardContentDesc = "This is your personal invitation to this year's Engineering Ball - Hollywood Glamour."
+                                DialogTitle = "Engineering Ball"
+                                DialogImage = {EngineeringBallBanner}
+                                DialogDesc = "Get your sleek suits and dazzling dresses out for the classiest night of the year! This is your personal invitation to HOLLYWOOD GLAMOUR. Come join us for unlimited alcohol (including spirits and champagne), endless dancing, and exquisite meals."
+                                DialogDate = "8 September 2022"
+                                DialogTime = "5:00 pm"
+                                VenueLink = "https://www.google.com/maps/place/Melbourne+Pavillion/@-37.7892068,144.9341968,17z/data=!3m1!4b1!4m5!3m4!1s0x6ad65d1ed7bcd833:0x207ba20d64e7b486!8m2!3d-37.7891529!4d144.9361857"
+                                DialogVenue = "Melbourne Pavillion"
+                                TicketLink = "https://events.humanitix.com/rmit-engineering-ball-2022?fbclid=IwAR32IKMVzSrsXQslcjaqQrJGZp1dftUPbDutdJ0n53PAQy8zcVHEZZEqeGI"
+                            />
+                        </div>
+
+                        <div class = 'eventsColumn'>
+                            <EventCards 
+                                cardImage = {NetworkingNightBanner}
+                                cardImageTitle = "Networking Night Banner"
+                                cardContentTitle = "Networking Night"
+                                cardContentDesc = "Females+ in RMIT Engineering is excited to announce our Exclusive Networking Night!"
+                                DialogTitle = "Exclusive Networking Night"
+                                DialogImage = {NetworkingNightBanner}
+                                DialogDesc = "Females+ in RMIT Engineering is excited to announce our Exclusive Networking Night! This is your opportunity to connect with industry professionals from 15 leading organisations over canapés and drinks in a relaxed professional setting. Market yourself, your uniqueness, share ideas and grab opportunities."
+                                DialogDate = "31 August 2022"
+                                DialogTime = "5:00 pm - 7:00 pm"
+                                VenueLink = "https://www.google.com/maps/place/113+Swanston+St,+Melbourne+VIC+3000,+Australia/@-37.814975,144.965895,16z/data=!4m5!3m4!1s0x6ad642b5c366a7ed:0xdf015a43a8c2f167!8m2!3d-37.8149747!4d144.9658948?hl=en-IN"
+                                DialogVenue = "RMIT, The Capitol"
+                                TicketLink = "https://events.humanitix.com/fire-exclusive-networking-night"
+                            />
+                        </div>
+
+                    </div>
+
+                    <br />
+                    <br />
+                </div>
             </div>
         </div>
     );
