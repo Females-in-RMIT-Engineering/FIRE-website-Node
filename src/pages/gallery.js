c2155e61cb235e3341232e14cd2b16ce3521a74f
AkshitaAgr <s3933878@student.rmit.edu.au>
2022-07-15 23:18:55 +1000

    edit 19

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -3,117 +3,19 @@
 import React, { useState, useCallback } from 'react';
 import { Helmet } from "react-helmet";
 import "./pagesCSS/gallery.css";
-//import { render } from "react-dom";
 import Gallery from "react-photo-gallery";
 import Carousel, { Modal, ModalGateway } from "react-images";
 import axios from "axios";
-
-/*import boat1 from "./pagesResources/images/boat-party-1.jpg";
-import boat2 from "./pagesResources/images/boat-party-2.jpg";
-import boat3 from "./pagesResources/images/boat-party-3.jpg";
-import boat4 from "./pagesResources/images/boat-party-4.jpg";
-import boat5 from "./pagesResources/images/boat-party-5.jpg";
-import boat6 from "./pagesResources/images/boat-party-6.jpg";
-import boat7 from "./pagesResources/images/boat-party-7.jpg";
-import welcome1 from "./pagesResources/images/welcome-night-1.jpg";
-import welcome2 from "./pagesResources/images/welcome-night-2.jpg";
-import welcome3 from "./pagesResources/images/welcome-night-3.jpg";
-import welcome4 from "./pagesResources/images/welcome-night-4.jpg";
-import welcome5 from "./pagesResources/images/committeePicture.jpg";
-import industry1 from "./pagesResources/images/industry-night-1.jpg";
-import industry2 from "./pagesResources/images/industry-night-2.jpg";
-import openDay1 from "./pagesResources/images/open-day-1.jpg";
-import studySession1 from "./pagesResources/images/study-session-1.jpg"; */
+import LoadingSign from "./pagesResources/loading sign/load.gif";
   
 const ImageGallery = () => {
-
- /* const photos = [
-    {
-      src: boat1,
-      width: 3,
-      height: 2
-    },
-    {
-      src: boat2,
-      width: 3,
-      height: 2
-    },
-    {
-      src: industry1,
-      width: 3,
-      height: 2
-    },
-    {
-      src: industry2,
-      width: 3,
-      height: 2
-    },
-    {
-      src: welcome1,
-      width: 4,
-      height: 3
-    },
-    {
-      src: welcome2,
-      width: 4,
-      height: 3
-    },
-    {
-      src: boat3,
-      width: 3,
-      height: 2
-    },
-    {
-      src: boat4,
-      width: 3,
-      height: 2
-    },
-    {
-      src: boat5,
-      width: 3,
-      height: 2
-    },
-    {
-      src: boat6,
-      width: 3,
-      height: 2
-    },
-    {
-      src: boat7,
-      width: 2,
-      height: 1.5
-    },
-    {
-      src: welcome3,
-      width: 4,
-      height: 3
-    },
-    {
-      src: welcome4,
-      width: 3,
-      height: 4
-    },
-    {
-      src: welcome5,
-      width: 4,
-      height: 3
-    },
-    {
-      src: openDay1,
-      width: 2,
-      height: 1.5
-    },
-    {
-      src: studySession1,
-      width: 4,
-      height: 3
-    }
-  ]; */
 
   const [images, setImages] = React.useState(null);
 
   const [currentImage, setCurrentImage] = useState(0);
   const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+  const [isLoading, setLoading] = useState(true);
 
   const openLightbox = useCallback((event, { photo, index }) => {
     setCurrentImage(index);
@@ -132,6 +34,7 @@ const ImageGallery = () => {
       const response = await axios.get(
         "https://google-photos-album-demo2.glitch.me/XKuAkPmTQ5Q5kDwF6"
       );
+      setLoading(false);
       if (!shouldCancel && response.data && response.data.length > 0) {
         setImages(
           response.data.map(url => ({
@@ -139,7 +42,8 @@ const ImageGallery = () => {
             thumbnail: `${url}=w100`,
             src: `${url}`,
             width: 7,
-            height: 5
+            height: 5,
+            title: "Image"
           }))
         );
       }
@@ -148,9 +52,37 @@ const ImageGallery = () => {
     return () => (shouldCancel = true);
   }, []);
 
-  const gallery = images ? <Gallery photos={images} onClick={openLightbox} /> : null;
+  const gallery = images ? <Gallery photos={images} onClick={openLightbox} isLoading ={isLoading} /> : null;
 
-  return (
+  return isLoading ? (
+    <div>
+      <Helmet>
+            <title>FIRE+ | Gallery</title>
+      </Helmet>
+
+      <div class = 'galleryContent'>
+
+        <br />
+
+        <h1 style={{ marginBottom: "-10px" }}>Gallery</h1>
+
+        <p class = 'galleryheading'>
+          Past Events Gallery
+        </p>
+
+        <div class = 'galleryLoadingSign'>
+          <img src= {LoadingSign} alt='Loading...' />
+        </div>
+
+        <br />
+
+      </div>
+
+      
+    </div>
+  ) 
+  :
+  (
     <div>
       <Helmet>
             <title>FIRE+ | Gallery</title>
@@ -194,19 +126,3 @@ const ImageGallery = () => {
 };
 
 export default ImageGallery;
-
-/* <Gallery photos={photos} onClick={openLightbox}/>
-          <ModalGateway>
-            {viewerIsOpen ? (
-              <Modal onClose={closeLightbox}>
-                <Carousel
-                  currentIndex={currentImage}
-                  views={photos.map(x => ({
-                    ...x,
-                    srcset: x.srcSet,
-                    caption: x.title
-                  }))}
-                />
-              </Modal>
-            ) : null}
-          </ModalGateway> */
